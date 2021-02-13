b2c86e0129e0a8ffb42766fde6d81c00a31781f0
Aman <aman54kumar@gmail.com>
2021-02-13 10:01:56 +0530

    navLinks prod file added

---
+++ src/Components/Generic/Header/HeaderMenu/NavLinksProd.js
@@ -0,0 +1,19 @@
+const navLinks = [
+  {
+    id: 0,
+    title: "Home",
+    path: "/arvebot-frontend/",
+  },
+  {
+    id: 1,
+    title: "About",
+    path: "/arvebot-frontend/about",
+  },
+  {
+    id: 2,
+    title: "Inheritance Calculator",
+    path: "/arvebot-frontend/calculator",
+  },
+];
+
+export default navLinks;
