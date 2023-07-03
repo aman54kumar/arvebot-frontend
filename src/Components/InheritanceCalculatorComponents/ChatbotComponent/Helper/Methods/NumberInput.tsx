142ee4ac34e53ea1f35fdca3dbbae3f65a6c3651
Aman <aman54kumar@gmail.com>
2023-07-04 02:21:48 +0530

    added numberInput values to 40, handling alphanumeric

--- src/Components/InheritanceCalculatorComponents/ChatbotComponent/Helper/Methods/NumberInput.tsx
+++ src/Components/InheritanceCalculatorComponents/ChatbotComponent/Helper/Methods/NumberInput.tsx
@@ -19,16 +19,47 @@ const numberInputTypesObject = {
     17: ['17', 'seventeen', 'sytten', 'søtten'],
     18: ['18', 'eighteen', 'atten'],
     19: ['19', 'nineeen', 'nitten'],
-    20: ['20', 'twenty', 'tyve', 'tjue'],
-    21: ['21'],
-    // until 40.
+    21: ['21', 'twenty one', 'tjue en'],
+    22: ['22', 'twenty two', 'tjue to'],
+    23: ['23', 'twenty three', 'tjue tre'],
+    24: ['24', 'twenty four', 'tjue fire'],
+    25: ['25', 'twenty five', 'tjue fem'],
+    26: ['26', 'twenty six', 'tjue seks'],
+    27: ['27', 'twenty seven', 'tjue syv'],
+    28: ['28', 'twenty eight', 'tjue åtte'],
+    29: ['29', 'twenty nine', 'tjue ni'],
+    30: ['30', 'thirty', 'tretti'],
+    31: ['31', 'thirty one', 'tretti en'],
+    32: ['32', 'thirty two', 'tretti to'],
+    33: ['33', 'thirty three', 'tretti tre'],
+    34: ['34', 'thirty four', 'tretti fire'],
+    35: ['35', 'thirty five', 'tretti fem'],
+    36: ['36', 'thirty six', 'tretti seks'],
+    37: ['37', 'thirty seven', 'tretti syv'],
+    38: ['38', 'thirty eight', 'tretti åtte', 'tretti atte'],
+    39: ['39', 'thirty nine', 'tretti ni'],
+    40: ['40', 'forty', 'førti', 'forti'],
 };
 
+/**
+ * Returns the key from the dictionary based on the entered number text.
+ * @param chosenValue The entered number text.
+ * @returns The key corresponding to the entered number text, or the original value if no match is found.
+ */
 export const returnKeyFromEnteredNumberText = (chosenValue: string) => {
+    // Format the chosen value by converting to lowercase and removing non-alphanumeric characters.
+    const formattedChosenValue = chosenValue
+        .toLowerCase()
+        .replace(/[^a-z0-9]/g, '');
+
+    // Find the key in the dictionary `numberInputTypesObject` that matches the formatted chosen value.
     const key = Object.keys(numberInputTypesObject).find((key) =>
-        (numberInputTypesObject as any)[key].includes(
-            chosenValue.toLowerCase(),
+        (numberInputTypesObject as any)[key].some(
+            (value: string) =>
+                value.replace(/[^a-z0-9]/g, '') === formattedChosenValue,
         ),
     );
+
+    // Return the key if a match is found, otherwise return the original value.
     return key ?? chosenValue;
 };
